49c70c641609b8a9adc0350abdd9e1dcb73d1c45
Welliton Fernandes Leal <wellfernandes@me.com>
2025-02-09 18:00:16 -0300

    feat: added user controller

---
+++ backend/app/Http/Controllers/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller{
+    
+    public function index(){
+        return User::all();
+    }
+
+    public function show(User $user){
+        return response()->json($user);
+    }
+}
