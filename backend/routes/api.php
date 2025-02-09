c1139f1beb9c677fd0042251b8b92c8f9939699c
Welliton Fernandes Leal <wellfernandes@me.com>
2025-02-09 18:00:55 -0300

    feat: added user routes and tasks

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:sanctum')->get('/users', [UserController::class, 'index']);
+
+Route::middleware('auth:sanctum')->get('/users/{user}', [UserController::class, 'show']);
+
+Route::middleware('auth:sanctum')->apiResource('tasks', TaskController::class);
+
+Route::middleware('auth:sanctum')->get('/tasks/{task}', [TaskController::class, 'show']);
