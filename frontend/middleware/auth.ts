7d00deae02b80fbb3de1971c4e6971f631eb4f69
Welliton Fernandes Leal <wellfernandes@me.com>
2025-02-10 17:54:21 -0300

    feat: added middleware config

---
+++ frontend/middleware/auth.ts
@@ -0,0 +1,10 @@
+import { useAuthStore } from '~/stores/auth'
+
+export default defineNuxtRouteMiddleware((to, from) => {
+  const authStore = useAuthStore()
+
+
+  if (!authStore.isAuthenticated) {
+    return navigateTo('/login')
+  }
+})
