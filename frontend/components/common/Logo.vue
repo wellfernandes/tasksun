b6742a81826c1a84d93b7cd9324c7954ec7e2a68
Welliton Fernandes Leal <wellfernandes@me.com>
2025-02-10 17:52:04 -0300

    feat: added logo component

---
+++ frontend/components/common/Logo.vue
@@ -0,0 +1,21 @@
+<!-- components/common/Logo.vue -->
+<template>
+  <div class="logo-container">
+    <img src="@/assets/images/tasksun-logo.png" alt="TaskSun - Gerenciador de Tarefas" class="logo" />
+  </div>
+</template>
+
+<style scoped>
+.logo-container {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  height: 100%;
+  padding-left: 50%;
+}
+
+.logo {
+  max-width: 70%;
+  height: auto;
+}
+</style>
