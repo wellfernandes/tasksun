3fc48eae860351643a3df5ecf498360550b6b7d3
Welliton Fernandes Leal <wellfernandes@me.com>
2025-02-10 13:48:34 -0300

    feat: added initial project

---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,28 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+  
+  // Adicione CSS do Vuetify
+  css: [
+    'vuetify/lib/styles/main.css',
+    '@mdi/font/css/materialdesignicons.css'
+  ],
+  
+  // Configurações de build
+  build: {
+    transpile: ['vuetify']
+  },
+  
+  // Plugins
+  plugins: [
+    '~/plugins/vuetify.js'
+  ],
+  
+  // Configurações do Vite
+  vite: {
+    define: {
+      'process.env.DEBUG': false,
+    },
+  }
+})
