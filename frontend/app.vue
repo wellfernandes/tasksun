12ddf4fdf9e6bc2868d90aa6b9e4fdcf5cfe7da3
Welliton Fernandes Leal <wellfernandes@me.com>
2025-02-11 12:14:31 -0300

    feat: added call for auth check

--- frontend/app.vue
+++ frontend/app.vue
@@ -1,8 +1,17 @@
+<!-- app.vue -->
 <template>
   <div>
-    <NuxtPage />  
+    <NuxtPage />
   </div>
 </template>
 
 <script setup>
+import { onMounted } from 'vue';
+import { useAuthStore } from '~/stores/auth';
+
+const authStore = useAuthStore();
+
+onMounted(() => {
+  authStore.init();
+});
 </script>
